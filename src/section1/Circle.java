843a1c1d942dedfc143a9602b881f233aa7bbd31
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-04 14:48:12 +0700

    Create Circle.java

---
+++ src/section1/Circle.java
@@ -0,0 +1,15 @@
+package section1;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double perimeter(){
+        return 2 * Math.PI * radius;
+    }
+    public double area(){
+        return Math.PI * radius * radius;
+    }
+}
