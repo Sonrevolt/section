7622066be56e1efb25c366d4f4dccce423af0c7c
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-04 14:12:41 +0700

    Update class TinhToan

--- src/section1/TinhToan.java
+++ src/section1/TinhToan.java
@@ -1,5 +1,33 @@
 package section1;
 
 public class TinhToan {
-    
+    public int Tong(int a, int b){
+        return a + b;
+    }
+    public double Tong(double a, double b){
+        return a + b;
+    }
+    public int Hieu(int a, int b){
+        return a - b;
+    }
+    public double Hieu(double a, double b){
+        return a - b;
+    }
+    public int Tich(int a, int b){
+        return a * b;
+    }
+    public double Tich(double a, double b){
+        return a * b;
+    }
+    public int Thuong(int a, int b){
+        if (b == 0)
+            throw new ArithmeticException("Khong chia cho 0");
+        return a / b;
+    }
+
+    public double Thuong(double a, double b){
+        if (b == 0)
+            throw new ArithmeticException("Khong chia cho 0");
+        return a / b;
+    }
 }
