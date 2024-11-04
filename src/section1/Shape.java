9987de914635d140c6d3cec7c522df4eca48c7a5
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-04 16:14:25 +0700

    Fix can re-input when error

--- src/section1/Shape.java
+++ src/section1/Shape.java
@@ -3,37 +3,67 @@ import java.util.Scanner;
 public class Shape {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        try {
-            System.out.print("Edge of the square: ");
-            double edge = sc.nextDouble();
-            Square square = new Square(edge);
-            System.out.println("Square perimeter = " + square.perimeter());
-            System.out.println("Square area = " + square.area());
 
-            System.out.print("Radius of the circle: ");
-            double radius = sc.nextDouble();
-            Circle circle = new Circle(radius);
-            System.out.println("Circle perimeter = " + circle.perimeter());
-            System.out.println("Circle area = " + circle.area());
-
-            System.out.print("EdgeA of the RightTriangle: ");
-            double edgeA = sc.nextDouble();
-            System.out.print("EdgeB of the RightTriangle: ");
-            double edgeB = sc.nextDouble();
-            RightTriangle rightTriangle = new RightTriangle(edgeA, edgeB);
-            System.out.println("RightTriangle perimeter = " + rightTriangle.perimeter());
-            System.out.println("RightTriangle area = " + rightTriangle.area());
-
-            System.out.print("Length of the RightTriangle: ");
-            double length = sc.nextDouble();
-            System.out.print("Width of the RightTriangle: ");
-            double width = sc.nextDouble();
-            Rectangle rectangle = new Rectangle(length, width);
-            System.out.println("Rectangle perimeter = " + rectangle.perimeter());
-            System.out.println("Rectangle area = " + rectangle.area());
-        }
-        catch (IllegalArgumentException e) {
+        Square square = null;
+        do {
+            try {
+                System.out.print("Edge of the square: ");
+                double edge = sc.nextDouble();
+                square= new Square(edge);
+                System.out.println("Square perimeter = " + square.perimeter());
+                System.out.println("Square area = " + square.area());
+            } catch (IllegalArgumentException e) {
                 System.out.println("Error: " + e.getMessage());
+                sc.nextLine();
             }
+        } while (square == null);
+
+        Circle circle = null;
+        do {
+            try {
+                System.out.print("Radius of the circle: ");
+                double radius = sc.nextDouble();
+                circle = new Circle(radius);
+                System.out.println("Circle perimeter = " + circle.perimeter());
+                System.out.println("Circle area = " + circle.area());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+                sc.nextLine();
+            }
+        } while (circle == null);
+
+        RightTriangle rightTriangle = null;
+        do {
+            try {
+                System.out.print("EdgeA of the RightTriangle: ");
+                double edgeA = sc.nextDouble();
+                System.out.print("EdgeB of the RightTriangle: ");
+                double edgeB = sc.nextDouble();
+                rightTriangle = new RightTriangle(edgeA, edgeB);
+                System.out.println("RightTriangle perimeter = " + rightTriangle.perimeter());
+                System.out.println("RightTriangle area = " + rightTriangle.area());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error " + e.getMessage());
+                sc.nextLine();
+            }
+        } while (rightTriangle == null);
+
+        Rectangle rectangle = null;
+        do {
+            try {
+                System.out.print("Length of the Rectangle: ");
+                double length = sc.nextDouble();
+                System.out.print("Width of the Rectangle: ");
+                double width = sc.nextDouble();
+                rectangle = new Rectangle(length, width);
+                System.out.println("Rectangle perimeter: " + rectangle.perimeter());
+                System.out.println("Rectangle area: " + rectangle.area());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error " + e.getMessage());
+                sc.nextLine();
+            }
+        } while (rectangle == null);
+
+        sc.close();
     }
 }
