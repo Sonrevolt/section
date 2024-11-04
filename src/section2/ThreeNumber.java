199c514545a0d7f8d3f43faf25c47552f7599a97
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-04 16:49:54 +0700

    Done finding smallest biggest number

--- src/section2/ThreeNumber.java
+++ src/section2/ThreeNumber.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class ThreeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
         int a = sc.nextInt();
+
+        System.out.print("Enter b: ");
         int b = sc.nextInt();
+
+        System.out.print("Enter c: ");
         int c = sc.nextInt();
         if (a >= b && a >= c) {
             System.out.println("The biggest number is : " + a);
@@ -16,9 +22,9 @@ public class ThreeNumber {
         if (a <= b && a <= c) {
             System.out.println("The smallest number is : " + a);
         } else if (b <= c) {
-            System.out.println("The biggest number is : " + b);
+            System.out.println("The smallest number is : " + b);
         } else {
-            System.out.println("The biggest number is : " + c);
+            System.out.println("The smallest number is : " + c);
         }
     }
 }
