17c21362257cff1a8b529037772dd396a964ccb2
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-05 17:49:02 +0700

    Create getter setter

--- src/section6/trungtamjava/model/School.java
+++ src/section6/trungtamjava/model/School.java
@@ -1,4 +1,38 @@
 package section6.trungtamjava.model;
 
 public class School {
+    private int id;
+    private String schoolName;
+    private String address;
+    public School(){}
+
+    public School(int id, String schoolName, String address){
+        this.id = id;
+        this.schoolName = schoolName;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
