3b0cc774f78961689fc4e7ef8d4096a8b6d0fd09
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-05 09:30:57 +0700

    Done Menu.java

--- src/section3/Menu.java
+++ src/section3/Menu.java
@@ -1,9 +1,36 @@
 package section3;
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Menu {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        
+        int choice = 0;
+
+        while (choice != 3) {
+            System.out.println(" MENU ");
+            System.out.println("1. Watch Menu");
+            System.out.println("2. Order");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option:  ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("You choose option 1");
+                    break;
+                case 2:
+                    System.out.println("You choose option 2");
+                    break;
+                case 3:
+                    System.out.println("Exit program");
+                    break;
+                default:
+                    System.out.println("Invalid option.Please re-enter");
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
