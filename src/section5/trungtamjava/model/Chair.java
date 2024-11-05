8b8e838b44f7a775f6894cfe3d6f6bbe63ca9f8a
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-05 15:05:46 +0700

    Done Chair.java

--- src/section5/trungtamjava/model/Chair.java
+++ src/section5/trungtamjava/model/Chair.java
@@ -1,4 +1,40 @@
 package section5.trungtamjava.model;
 
 public class Chair {
+    private String material;
+    private String color;
+    private int weight;
+
+    public Chair() {
+    }
+
+    public Chair(String material, String color, int weight) {
+        this.material = material;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeightCapacity() {
+        return weight;
+    }
+
+    public void setWeightCapacity(int weight) {
+        this.weight = weight;
+    }
 }
