454b4a282a8908009163747fd138e3f339a1f455
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-05 11:15:32 +0700

    Test count character

--- src/section4/String1.java
+++ src/section4/String1.java
@@ -1,10 +1,10 @@
 package section4;
-
-
+import java.util.Scanner;
 
 public class String1 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String s1 = "Hello, World!";
         System.out.println("String length: " + s1.length());
 
@@ -14,6 +14,22 @@ public class String1 {
         }else{
             System.out.println("Not equals");
         }
+
+        int count = 0;
+        char character =0;
+        for (int i = 0; i < s1.length(); i++) {
+            char currentChar = s1.charAt(i);
+            if (currentChar == character) {
+                count++;
+            }
+        }
+        System.out.print("Nhập ký tự cần đếm: ");
+        char characterToCount = scanner.next().charAt(0);
+
+        // In kết quả
+        System.out.println("Số ký tự '" + characterToCount + "' trong chuỗi là: " + count);
+
+        scanner.close();
     }
 }
 
