b048c74e7ae790383117651ea1ef31ab522e1d68
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-05 15:50:16 +0700

    Create and done MainStudent and MainTeacher

---
+++ src/section5/trungtamjava/main/MainStudent.java
@@ -0,0 +1,9 @@
+package section5.trungtamjava.main;
+import section5.trungtamjava.model.*;
+public class MainStudent {
+    public static void main(String[] args){
+        Student student = new Student();
+        student.input();
+        student.info();
+    }
+}
---
+++ src/section5/trungtamjava/main/MainTeacher.java
@@ -0,0 +1,9 @@
+package section5.trungtamjava.main;
+import section5.trungtamjava.model.*;
+public class MainTeacher {
+    public static void main(String[] args){
+        Teacher teacher = new Teacher();
+        teacher.input();
+        teacher.info();
+    }
+}
