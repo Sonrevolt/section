85d49cb7c02de69a2ac72b96c61da5db2526eb82
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-05 10:51:00 +0700

    Test string equals

--- src/section4/String1.java
+++ src/section4/String1.java
@@ -8,8 +8,12 @@ public class String1 {
         String s1 = "Hello, World!";
         System.out.println("String length: " + s1.length());
 
-        String s2 = "Hello, World!, Hihi";
-        
+        String s2 = "Hello, World!";
+        if(s2.equals(s1)){
+            System.out.println("equals");
+        }else{
+            System.out.println("Not equals");
+        }
     }
 }
 
