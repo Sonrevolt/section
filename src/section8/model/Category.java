b1d100aab3fdb83a62dc3b5e590dd3752531cabb
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-06 16:22:27 +0700

    Create and done Category and Product.java

--- src/section8/model/Category.java
+++ src/section8/model/Category.java
@@ -1,4 +1,25 @@
 package section8.model;
 
+
 public class Category {
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category ID: " + id + ", Name: " + name;
+    }
 }
--- src/section8/model/Product.java
+++ src/section8/model/Product.java
@@ -1,4 +1,28 @@
 package section8.model;
 
 public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private Category category;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product ID: " + id + ", Name: " + name + ", Price: " + price + ", " + category;
+    }
 }
+
