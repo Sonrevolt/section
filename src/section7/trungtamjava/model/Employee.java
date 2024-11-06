8594f23e1c66fc3f1dc24110a7c6c6955b28eac2
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-06 10:57:10 +0700

    Fix employee

--- src/section7/trungtamjava/model/Employee.java
+++ src/section7/trungtamjava/model/Employee.java
@@ -2,12 +2,10 @@ package section7.trungtamjava.model;
 
 import java.util.Scanner;
 
-public class Employee {
-    private int id;
-    private String name;
-    private int age;
-
-    public Employee(){}
+public abstract class Employee {
+    protected int id;
+    protected String name;
+    protected int age;
 
     public Employee(int id, String name, int age){
         this.id = id;
@@ -15,44 +13,5 @@ public class Employee {
         this.age = age;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void input() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter ID: ");
-        id = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter Name: ");
-        name = sc.nextLine();
-        System.out.print("Enter Age: ");
-        age = sc.nextInt();
-    }
-
-    public void info() {
-        System.out.println("ID: " + id);
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-    }
+    public abstract void info();
 }
