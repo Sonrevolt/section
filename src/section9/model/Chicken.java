383f6dacddd581c43a56ae52da9450e8b4c2cccb
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-07 08:57:19 +0700

    Done Animal, Chicken, Dog.java

--- src/section9/model/Chicken.java
+++ src/section9/model/Chicken.java
@@ -1,4 +1,20 @@
 package section9.model;
 
-public class Chicken {
+public class Chicken extends Animal {
+    private boolean canLayEggs;
+
+    public Chicken(){}
+
+    public Chicken(String name, int age, boolean canLayEggs){
+        super(name, age);
+        this.canLayEggs = canLayEggs;
+    }
+
+    public boolean isCanLayEggs() {
+        return canLayEggs;
+    }
+
+    public void setCanLayEggs(boolean canLayEggs) {
+        this.canLayEggs = canLayEggs;
+    }
 }
--- src/section9/model/Dog.java
+++ src/section9/model/Dog.java
@@ -1,4 +1,19 @@
 package section9.model;
 
-public class Dog {
+public class Dog extends  Animal {
+    private String breed;
+    public Dog(){}
+
+    public Dog(String name, int age, String breed){
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
