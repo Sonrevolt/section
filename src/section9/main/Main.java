b74cb3dea66f37b628d4026bcfef623a93607acd
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-07 09:10:03 +0700

    Create Main class

---
+++ src/section9/main/Main.java
@@ -0,0 +1,16 @@
+package section9.main;
+
+import section9.model.Animal;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner sc = new Scanner(System.in);
+    public static void input(Animal a){
+        System.out.println("Enter name: ");
+        a.setName(sc.nextLine());
+        System.out.println("Enter age: ");
+        a.setAge(sc.nextInt());
+        if()
+    }
+}
