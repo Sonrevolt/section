6ba9bf25aa44709430bb038ebcf242d4051b7c7c
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-08 15:30:38 +0700

    Update Car.java

--- src/section9/model/Car.java
+++ src/section9/model/Car.java
@@ -5,6 +5,7 @@ public class Car extends Vehicle {
     public Car(){}
     public Car(double price, int numberOfSeats, String type){
         super(price, numberOfSeats);
+        this.type = type;
     }
 
     public String getType() {
