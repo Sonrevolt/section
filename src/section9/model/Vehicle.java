65a0b0fd545e2a8230f88fd488f411a278061e1c
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-08 15:55:43 +0700

    Update Vehicle.java

--- src/section9/model/Vehicle.java
+++ src/section9/model/Vehicle.java
@@ -5,24 +5,19 @@ public class Vehicle {
     private int numberOfSeats;
 
     public Vehicle(){}
-
     public Vehicle(double price,int numberOfSeats) {
         this.price = price;
         this.numberOfSeats = numberOfSeats;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     public int getNumberOfSeats() {
         return numberOfSeats;
     }
-
     public void setNumberOfSeats(int numberOfSeats) {
         this.numberOfSeats = numberOfSeats;
     }
