7a3d3ad23e6424e6a3f77dc4bced0eb4529f3cc2
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-11 09:14:32 +0700

    Done Person1.java

--- src/section10/Person1.java
+++ src/section10/Person1.java
@@ -6,14 +6,20 @@ abstract class Person{
 class Employee extends Person{
     @Override
     void salary() {
-        System.out.println("Employee salary: 5 millions");
+        System.out.println("Employee salary : 5 millions");
     }
 }
 class Boss extends  Person{
     @Override
+    void salary() {
+        System.out.println("Boss salary : 0 + luong trach nhiem va hoa hong  ");
+    }
 }
 public class Person1 {
     public static void main(String[] args){
-
+        Employee employee = new Employee();
+        Boss boss = new Boss();
+        employee.salary();
+        boss.salary();
     }
 }
