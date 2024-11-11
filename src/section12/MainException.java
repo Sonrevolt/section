b6812f946d80a5db50facc0d885c75900c6fb748
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-11 14:46:20 +0700

    Create MainException.java

---
+++ src/section12/MainException.java
@@ -0,0 +1,16 @@
+package section12;
+
+import java.util.Scanner;
+
+public class MainException {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = getInput("Enter x: ", sc);
+        int y = getInput("Enter y: ", sc);
+        System.out.println("x + y = " +(x+y));
+    }
+    public static int getInput(String prompt, Scanner sc){
+        int number = 0;
+        boolean
+    }
+}
