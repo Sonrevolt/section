f60d19cb7046e49bf8633f1ce28e0f7788a0eb77
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2024-11-11 14:14:21 +0700

    Create and done Employee.java

---
+++ src/section11/Employee.java
@@ -0,0 +1,23 @@
+package section11;
+
+class Employee {
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public double salary() {
+        return 50000000;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
